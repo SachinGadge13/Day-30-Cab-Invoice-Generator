98b3d83b864ea0f06916d6894a6a24c318b3ccf0
SachinGadge13 <gadges14@gmail.com>
2022-10-28 00:36:36 +0530

    Added:Program to calculate Fare

--- src/main/java/com/bridgelabz/CensusAnalyser/Day_30_Cab_Invoice_Generator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CensusAnalyser/Day_30_Cab_Invoice_Generator/InvoiceGenerator.java
@@ -7,15 +7,8 @@ public class InvoiceGenerator {
     private static final double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return Math.max(MINIMUM_FARE, distance * COST_PER_KM + time * COST_PER_TIME);
-    }
 
-    public double calculateFare(Ride[] rides) {
-        double totalFare = 0;
-        for (Ride ride : rides) {
-            totalFare += calculateFare(ride.getDistance(), ride.getTime());
-        }
-        return totalFare;
+        return Math.max(MINIMUM_FARE, distance * COST_PER_KM + time * COST_PER_TIME);
     }
 
 }
--- src/test/java/com/bridgelabz/CensusAnalyser/Day_30_Cab_Invoice_Generator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyser/Day_30_Cab_Invoice_Generator/InvoiceGeneratorTest.java
@@ -2,19 +2,12 @@ package com.bridgelabz.CensusAnalyser.Day_30_Cab_Invoice_Generator;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
-    InvoiceGenerator invoiceGenerator;
-
-    @Before
-    public void init() {
-        invoiceGenerator = new InvoiceGenerator();
-    }
-
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 1.0;
         int time = 10;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -23,16 +16,10 @@ public class InvoiceGeneratorTest {
 
     @Test
     public void givenDistanceAndTime_WhenTotalFareLessThan10_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         assertEquals(5, fare, 0.0);
-    }
-
-    @Test
-    public void givenMultipleRidesShouldReturnTotalFare() {
-        Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
-        double fare = invoiceGenerator.calculateFare(rides);
-        assertEquals(30, fare, 0.0);
     }
 }
